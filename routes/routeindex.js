e07f8e921b0f3e62da82c6437740b5a1a3fdb15c
GUPABE10 <GUPABE10@gmail.com>
2022-01-28 19:43:23 -0600

    Lab 8 done

--- routes/routeindex.js
+++ routes/routeindex.js
@@ -1,57 +1,51 @@
-
 const express = require('express');
 const router = express.Router();
+//const Task = require('../model/task');
 const Post = require('../model/post');
 
-
 router.get('/', async function(req,res){
-
-  let posts = await Post.find()
-
-  res.render('index',{posts});
+  let postes = await Post.find()
+  console.log(postes)
+  res.render('index', {postes})
 });
-
 
 router.get('/newPost', async (req,res) =>{
   res.render('newPost');
 });
 
 router.post('/newPost', async (req,res) =>{
-
   let post = new Post(req.body)
+  console.log(post)
   await post.save()
-  res.redirect("/")
+  res.redirect('/')
 });
 
 
-router.get('/edit/:id', async (req,res) =>{
+//edit
+router.get('/edit/:id',   async(req,res) =>{
   let id = req.params.id
   let post = await Post.findById(id)
+  res.render('edit', {post})
+})
 
-  res.render('edit',{post});
-});
-
-
-// # PENDIENTE
-router.post('/edit/:id', async (req,res) =>{
-
-  res.redirect("/")
-});
-
-// # PENDIENTE
-router.post('/delete/:id', async (req,res) =>{
-
-  res.redirect("/")
-});
+router.post('/edit/:id',   async(req,res) =>{
+  await Post.updateOne({_id:req.params.id},req.body)
+  res.redirect('/')
+})
 
 
-router.get('/delete/:id', async (req,res) =>{
+//delete
+router.get('/delete/:id',   async(req,res) =>{
   let id = req.params.id
   let post = await Post.findById(id)
+  res.render('delete', {post})
+})
 
-  res.render('delete',{post});
-});
-
+router.post('/delete/:id',   async(req,res) =>{
+  let id = req.params.id
+  await Post.remove({_id:id})
+  res.redirect('/')
+})
 
 
 module.exports = router;
